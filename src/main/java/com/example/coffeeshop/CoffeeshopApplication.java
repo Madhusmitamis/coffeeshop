a267c3ee2d38884e81e99d62e10ef6cc27be075e
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-09-07 17:28:27 +0300

    Save local changes before pull

--- src/main/java/com/example/coffeeshop/CoffeeshopApplication.java
+++ src/main/java/com/example/coffeeshop/CoffeeshopApplication.java
@@ -2,11 +2,10 @@ package com.example.coffeeshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CoffeeshopApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeshopApplication.class, args);
 	}
